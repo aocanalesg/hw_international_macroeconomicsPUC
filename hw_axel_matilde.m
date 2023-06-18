fe01da008a340f3d11792788a959afc13c1d61bb
Matilde Cerda <matildeguadalupecerda@gmail.com>
2023-06-17 20:16:53 -0600

    17 de junio a las 8 16
    Se removio tendencia por todos los metodos solicitados. Se comenzara a realizar correlacioens

--- hw_axel_matilde.m
+++ hw_axel_matilde.m
@@ -55,56 +55,100 @@ X_usa= X_usa_gdp.*GDP_pc_usa;
 %%%%%%%%%%% Cycle-trend decomposition %%%%%%%%%%%
 %NIC
 %lambda=100
-[trend_y_pc_nic, cycle_nic_1]   = hpfilter(log(GDP_pc_nic),100);
-[trend_c_nic, cycle_nic_2]       = hpfilter(log(C_nic),100);
-[trend_i_nic, cycle_nic_3]   = hpfilter(log(I_nic),100);
-[trend_g_nic, cycle_nic_4]       = hpfilter(log(G_nic),100);
-[trend_tb_nic, cycle_nic_5]       = hpfilter((X_nic-M_nic)./trend_y_pc_nic,100);
-[trend_gy_nic, cycle_nic_6]       = hpfilter(log(G_nic)./log(GDP_pc_nic),100);
+[trend_y_pc_nic, hp_cycle_nic_1]   = hpfilter(log(GDP_pc_nic),100);
+[~, hp_cycle_nic_2]       = hpfilter(log(C_nic),100);
+[~, hp_cycle_nic_3]   = hpfilter(log(I_nic),100);
+[~, hp_cycle_nic_4]       = hpfilter(log(G_nic),100);
+[~, hp_cycle_nic_5]       = hpfilter((X_nic-M_nic)./trend_y_pc_nic,100);
+[~, hp_cycle_nic_6]       = hpfilter(log(G_nic)./log(GDP_pc_nic),100);
 
 %lambda=6.25
-[trend_y_pc_nic, cycle_nic_7]   = hpfilter(log(GDP_pc_nic),6.25);
-[trend_c_nic, cycle_nic_8]       = hpfilter(log(C_nic),6.25);
-[trend_i_nic, cycle_nic_9]   = hpfilter(log(I_nic),6.25);
-[trend_g_nic, cycle_nic_10]       = hpfilter(log(G_nic),6.25);
-[trend_tb_nic, cycle_nic_11]       = hpfilter((X_nic-M_nic)./trend_y_pc_nic,6.25);
-[trend_gy_nic, cycle_nic_12]       = hpfilter(log(G_nic)./log(GDP_pc_nic),6.25);
+[trend_y_pc_nic, hp_cycle_nic_7]   = hpfilter(log(GDP_pc_nic),6.25);
+[~, hp_cycle_nic_8]       = hpfilter(log(C_nic),6.25);
+[~, hp_cycle_nic_9]   = hpfilter(log(I_nic),6.25);
+[~, hp_cycle_nic_10]       = hpfilter(log(G_nic),6.25);
+[~, hp_cycle_nic_11]       = hpfilter((X_nic-M_nic)./trend_y_pc_nic,6.25);
+[~, hp_cycle_nic_12]       = hpfilter(log(G_nic)./log(GDP_pc_nic),6.25);
 
 
 %USA
 %lambda=100
-[trend_y_pc_usa, cycle_usa_1]   = hpfilter(log(GDP_pc_usa),100);
-[trend_c_usa, cycle_usa_2]       = hpfilter(log(C_usa),100);
-[trend_i_usa, cycle_usa_3]   = hpfilter(log(I_usa),100);
-[trend_g_usa, cycle_usa_4]       = hpfilter(log(G_usa),100);
-[trend_tb_usa, cycle_usa_5]       = hpfilter((X_usa-M_usa)./trend_y_pc_usa,100);
-[trend_gy_usa, cycle_usa_6]       = hpfilter(log(G_usa)./log(GDP_pc_usa),100);
+[trend_y_pc_usa_1, hp_cycle_usa_1]   = hpfilter(log(GDP_pc_usa),100);
+[~, hp_cycle_usa_2]       = hpfilter(log(C_usa),100);
+[~, hp_cycle_usa_3]   = hpfilter(log(I_usa),100);
+[~, hp_cycle_usa_4]       = hpfilter(log(G_usa),100);
+[~, hp_cycle_usa_5]       = hpfilter((X_usa-M_usa)./trend_y_pc_usa_1,100);
+[~, hp_cycle_usa_6]       = hpfilter(log(G_usa)./log(GDP_pc_usa),100);
 
 %lambda=6.25
-[trend_y_pc_usa, cycle_usa_7]   = hpfilter(log(GDP_pc_usa),6.25);
-[trend_c_usa, cycle_usa_8]       = hpfilter(log(C_usa),6.25);
-[trend_i_usa, cycle_usa_9]   = hpfilter(log(I_usa),6.25);
-[trend_g_usa, cycle_usa_10]       = hpfilter(log(G_usa),6.25);
-[trend_tb_usa, cycle_usa_11]       = hpfilter((X_usa-M_usa)./trend_y_pc_usa, 6.25);
-[trend_gy_usa, cycle_usa_12]       = hpfilter(log(G_usa)./log(GDP_pc_usa),6.25);
+[trend_y_pc_usa, hp_cycle_usa_7]   = hpfilter(log(GDP_pc_usa),6.25);
+[~, hp_cycle_usa_8]       = hpfilter(log(C_usa),6.25);
+[~, hp_cycle_usa_9]   = hpfilter(log(I_usa),6.25);
+[~, hp_cycle_usa_10]       = hpfilter(log(G_usa),6.25);
+[~, hp_cycle_usa_11]       = hpfilter((X_usa-M_usa)./trend_y_pc_usa, 6.25);
+[~, hp_cycle_usa_12]       = hpfilter(log(G_usa)./log(GDP_pc_usa),6.25);
 
 %%%%%%%%%%%%%%%%%%%%%%
 % 1.log linear detrending
-%
+%Nicaragua
+det1_gdp_pc_nic = detrend(log(GDP_pc_nic),1);
+det1_C_nic = detrend(log(C_nic),1);
+det1_I_nic = detrend(log(I_nic),1);
+det1_G_nic = detrend(log(G_nic),1);
+det1_tb_nic = detrend((X_nic-M_nic)./det1_gdp_pc_nic,1);
+det1_gy_nic = detrend(log(G_nic)./log(GDP_pc_nic),1);
+
+%USA
+det1_gdp_pc_usa = detrend(log(GDP_pc_usa),1);
+det1_C_usa = detrend(log(C_usa),1);
+det1_I_usa = detrend(log(I_usa),1);
+det1_G_usa = detrend(log(G_usa),1);
+det1_tb_usa = detrend((X_usa-M_usa)./det1_gdp_pc_usa,1);
+det1_gy_usa = detrend(log(G_usa)./log(GDP_pc_usa),1);
 
 %%%%%%%%%%%%%%%%%%%%%%
 % 2.log quadratic detrending
-%
+%Nicaragua 
+det2_gdp_pc_nic = detrend(log(GDP_pc_nic),2);
+det2_C_nic = detrend(log(C_nic),2);
+det2_I_nic = detrend(log(I_nic),2);
+det2_G_nic = detrend(log(G_nic),2);
+det2_tb_nic = detrend((X_nic-M_nic)./det1_gdp_pc_nic,2);
+det2_gy_nic = detrend(log(G_nic)./log(GDP_pc_nic),2);
+
+%USA
+det2_gdp_pc_usa = detrend(log(GDP_pc_usa),2);
+det2_C_usa = detrend(log(C_usa),2);
+det2_I_usa = detrend(log(I_usa),2);
+det2_G_usa = detrend(log(G_usa),2);
+det2_tb_usa = detrend((X_usa-M_usa)./det2_gdp_pc_usa,2);
+det2_gy_usa = detrend(log(G_usa)./log(GDP_pc_usa),2);
 
 %%%%%%%%%%%%%%%%%%%%%%
-% 3. HP
-% stD
+% 3. Correlatios per country and detrending method
+%Nicaragua
+%HP lambda = 100
+nic_ciclo = [hp_cycle_nic_1 hp_cycle_nic_3 hp_cycle_nic_4 hp_cycle_nic_5 hp_cycle_nic_6];
+nic_y_c = [hp_cycle_nic_1[35:62] hp_cycle_nic_2[35:62]];
+nic_corr_ciclo= corrcoef(nic_ciclo);
+
+%HP lambda = 6.25
+%log-linear detrending 
+%log-quadratic detrending
+
+%A = [x y 2*y+3];
+%R = corrcoef(A)
+
+
+
+
+
 
 %nic
 %LAMBDA=100
-for i=1:6
-fprintf('%5.2f \n ',  std(cycle_nic_i)*100)
-end
+%for i=1:6
+%fprintf('%5.2f \n ',  std(cycle_nic_i)*100)
+%end
 
 %LAMBDA=6.25
 
