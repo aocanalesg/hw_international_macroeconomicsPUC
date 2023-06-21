cc287a289112a50f589a1d240a496f7537635f7c
aocanalesg <135900750+aocanalesg@users.noreply.github.com>
2023-06-20 23:14:42 -0600

    20/06/2023 - 11:14
    Finalizo inciso 1

--- Problema1_Axel_Matilde.m
+++ Problema1_Axel_Matilde.m
@@ -324,11 +324,12 @@ hp_cycle_usa_6_std = (cellfun(@std,hp_usa_6)')./std(hp_cycle_usa_7); %Generates 
 
 results_std = [det1_cycle_nic_std, det1_cycle_usa_std, det2_cycle_nic_std, det2_cycle_usa_std, hp_cycle_nic_100_std, hp_cycle_usa_100_std, hp_cycle_nic_6_std, hp_cycle_usa_6_std]
 results_std(1,:) = [std(det1_gdp_pc_nic), std(det1_gdp_pc_usa), std(det2_gdp_pc_nic), std(det2_gdp_pc_usa), std(hp_cycle_nic_1), std(hp_cycle_usa_1), std(hp_cycle_nic_2), std(hp_cycle_usa_2)]%primera fila solo incluye la desviacion por si sola
+results_std(7,:) = [std(det1_tb_nic), std(det1_tb_usa), std(det2_tb_nic), std(det2_tb_usa), std(hp_cycle_nic_5), std(hp_cycle_usa_5), std(hp_cycle_nic_11), std(hp_cycle_usa_11)]%primera fila solo incluye la desviacion por si sola
 %results_std_round=varfun(@(x)round(x,2), results_std);
 
 %Export to excel
 
-rowNames1_std = {'s_y','s_c/s_y','s_g/s_y','s_i/s_y','s_x/s_y','s_m/s_y', 's_(tb)/s_y'};%"s" represents standard deviation
+rowNames1_std = {'s_y','s_c/s_y','s_g/s_y','s_i/s_y','s_x/s_y','s_m/s_y', 's_(tb)'};%"s" represents standard deviation
 colNames1_std = {'Linear Nic','Linear USA','Quadratic Nic','Quadratic USA', 'HP con λ = 100 Nic','HP con λ = 100 USA','HP con λ = 6.25 Nic','HP con λ = 6.25 USA'};
 results_std = array2table(results_std,'RowNames',rowNames1_std,'VariableNames',colNames1_std);
 results_std_round=varfun(@(x)round(x,2), results_std) %round to the nearest hundredth
@@ -473,19 +474,24 @@ writetable(results_auto,filename);
 %%%%% Graph showing the natural logarithm of real per capita GDP and the trend, one panel per trend.
 %%%%%% how the detrending method influences the volatility of the cyclical component of output.? 
 %%%%%% Nicaragua
-figure(1)
+fig(1) = figure('name', 'Nic','units','inch','position',[0,0,11,7.5]);
+
 subplot(2,2,1);
 x = 1960:2021;
 plot(x,log(GDP_pc_nic),x,trend_y_pc_nic,'-');
 xlabel('Periodo de Estudio');
 ylabel('Logaritmo');
+xlim([1960 2021]);
 legend('PIB per cápita a precios contantes ','Tendencia');
+legend('boxoff');
 title('HP filtering with λ = 100','FontSize', 12);
+
 
 subplot(2,2,2);
 plot(x,log(GDP_pc_nic),x,hp6_trend_y_pc_nic);
 xlabel('Periodo de Estudio');
 ylabel('Logaritmo');
+xlim([1960 2021]);
 title('HP filtering with λ = 6.25');
 
 subplot(2,2,3);
@@ -493,6 +499,7 @@ trend1_gdp_pc_nic = log(GDP_pc_nic) - det1_gdp_pc_nic;
 plot(x,log(GDP_pc_nic),x,trend1_gdp_pc_nic);
 xlabel('Periodo de Estudio');
 ylabel('Logaritmo');
+xlim([1960 2021]);
 title('Log-linear detrending')
 
 subplot(2,2,4);
@@ -500,7 +507,11 @@ trend2_gdp_pc_nic = log(GDP_pc_nic) - det2_gdp_pc_nic;
 plot(x,log(GDP_pc_nic),x,trend2_gdp_pc_nic);
 xlabel('Periodo de Estudio');
 ylabel('Logaritmo');
+xlim([1960 2021]);
 title('Log-quadratic detrending')
+
+
+
 
 %%%%%% USA
 %Following variables are created for the entire sample
@@ -512,19 +523,22 @@ det2_gdp_pc_usa_g = detrend(log(GDP_pc_usa1),2);
 
 
 
-figure(2)
+fig(2)= figure('name', 'USA','units','inch','position',[0,0,11,7.5]);
 subplot(2,2,1);
 x = 1965:2021;
 plot(x,log(GDP_pc_usa1),x,graph_trend_y_pc_usa,'-');
 xlabel('Periodo de estudio');
 ylabel('Logaritmo');
+xlim([1965 2021]);
 legend('PIB per cápita a precios contante ','Tendencia');
+legend('boxoff');
 title('HP filtering with λ = 100','FontSize', 12);
 
 subplot(2,2,2);
 plot(x,log(GDP_pc_usa1),x,graph6_trend_y_pc_usa);
 xlabel('Periodo de estudio');
 ylabel('Logaritmo');
+xlim([1965 2021]);
 title('HP filtering with λ = 6.25');
 
 subplot(2,2,3);
@@ -533,6 +547,7 @@ trend1_gdp_pc_usa = log(GDP_pc_usa1) - det1_gdp_pc_usa_g;
 plot(x,log(GDP_pc_usa1),x,trend1_gdp_pc_usa);
 xlabel('Periodo de estudio');
 ylabel('Logaritmo');
+xlim([1965 2021]);
 title('Log-linear detrending')
 
 subplot(2,2,4);
@@ -540,7 +555,13 @@ trend2_gdp_pc_usa = log(GDP_pc_usa1) - det2_gdp_pc_usa_g;
 plot(x,log(GDP_pc_usa1),x,trend2_gdp_pc_usa);
 xlabel('Periodo de estudio');
 ylabel('Logaritmo');
+xlim([1965 2021]);
 title('Log-quadratic detrending;')
+
+%%Export graphics
+
+exportgraphics(figure(1),'picture1.png','Resolution',700)
+exportgraphics(figure(2),'picture2.png','Resolution',700)
 
 %%%%%%% 1.C %%%%%%%%%%%
 %%%%% Using only the series of real GDP calculate the number of cycles that experienced each
@@ -549,3 +570,22 @@ title('Log-quadratic detrending;')
 
 %%%%%% Nicaragua
 
+fig(3)= figure('name', 'NIC_REAL_GDP','units','inch','position',[0,0,11,7.5]);
+plot([1960:2021], GDP_nic_constant, 'LineWidth',1.5);
+xregion(1977,1983);
+xregion(2008,2017);
+xlabel('Año');
+ylabel('Real GDP');
+xlim([1960 2021]);
+title('PIB real de Nicaragua 1960-2021');
+
+fig(4)= figure('name', 'USA_REAL_GDP','units','inch','position',[0,0,11,7.5]);
+plot([1965:2021], usa(:,8), 'LineWidth',1.5);
+xlabel('Año');
+ylabel('Real GDP');
+xlim([1965 2021]);
+title('PIB real de Estados Unidos 1965-2021');
+
+%export 
+exportgraphics(figure(3),'picture3.png','Resolution',700)
+exportgraphics(figure(4),'picture4.png','Resolution',700)
